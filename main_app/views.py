d572e6c024cbc49e6a505a886de919e517aed805
Jessaroo <j_caetana@yahoo.com>
2023-06-06 14:28:31 -0700

    adding css

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+# dummy tasks
+tasks = [
+    {'todo': 'wash towels', 'when': 'Today'},
+    {'todo': 'wash dishes', 'when': 'Tonight'},
+]
+
 # Create your views here.
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def tasks_index(request):
+  return render(request, 'tasks/index.html', {
+    'tasks': tasks
+  })
