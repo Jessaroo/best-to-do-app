15224ae1b1a54bc6015c13e6cc86c00a3b15f1ce
Jessaroo <j_caetana@yahoo.com>
2023-06-07 16:50:45 -0700

    adding signup feature

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 from .views import CategoryCreate
@@ -19,4 +20,5 @@ urlpatterns = [
     path('categories/<int:pk>/update/', views.CategoryUpdate.as_view(), name='category_update'),
     path('categories/<int:pk>/delete/', views.CategoryDelete.as_view(), name='category_delete'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
